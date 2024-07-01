2c41327ceb9d109d83aa66f4c816b06a4e5ca8f5
Gilang Aditia <gaditia744@gmail.com>
2024-07-01 15:29:51 +0700

    update

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { FaGraduationCap } from "react-icons/fa6";
+import { MdOutlineWorkspacePremium } from "react-icons/md";
+import { PiCertificate } from "react-icons/pi";
 
 const Resume = () => {
   return (
@@ -6,31 +9,31 @@ const Resume = () => {
       <header>
         <h2 className="h2 article-title">Resume</h2>
       </header>
-      <section className="resume-content mt-10">
+      {/* <section className="resume-content mt-10">
         <img
           src="https://github.com/Anmol-Baranwal/Cool-GIFs-For-GitHub/assets/74038190/d0cfe7d1-0b8c-4e4a-9a66-875290ba6065"
           alt=""
         />
-      </section>
-      {/* <section className="timeline">
+      </section> */}
+      <section className="timeline">
         <div className="title-wrapper">
           <div className="icon-box">
-            <ion-icon name="book-outline" />
+            <FaGraduationCap />
           </div>
-          <h3 className="h3">Education</h3>
+          <h3 className="h3">Pendidikan</h3>
         </div>
         <ol className="timeline-list">
           <li className="timeline-item">
             <h4 className="h4 timeline-item-title">
-              University school of the arts
+              Institut Teknologi Telkom Purwokerto
             </h4>
-            <span>2007 — 2008</span>
+            <span>2019 — 2023</span>
             <p className="timeline-text">
-              Nemo enims ipsam voluptatem, blanditiis praesentium voluptum
-              delenit atque corrupti, quos dolores et quas molestias exceptur.
+              Fakultas Informatika, Jurusan Software Engineering. IPK. 3,75
+              Cumlaude
             </p>
           </li>
-          <li className="timeline-item">
+          {/* <li className="timeline-item">
             <h4 className="h4 timeline-item-title">New york academy of art</h4>
             <span>2006 — 2007</span>
             <p className="timeline-text">
@@ -47,13 +50,13 @@ const Resume = () => {
               Duis aute irure dolor in reprehenderit in voluptate, quila
               voluptas mag odit aut fugit, sed consequuntur magni dolores eos.
             </p>
-          </li>
+          </li> */}
         </ol>
       </section>
       <section className="timeline">
         <div className="title-wrapper">
           <div className="icon-box">
-            <ion-icon name="book-outline" />
+            <MdOutlineWorkspacePremium />
           </div>
           <h3 className="h3">Experience</h3>
         </div>
@@ -84,7 +87,41 @@ const Resume = () => {
           </li>
         </ol>
       </section>
-      <section className="skill">
+      <section className="timeline">
+        <div className="title-wrapper">
+          <div className="icon-box">
+            <PiCertificate />
+          </div>
+          <h3 className="h3">Sertifikasi</h3>
+        </div>
+        <ol className="timeline-list">
+          <li className="timeline-item">
+            <h4 className="h4 timeline-item-title">Creative director</h4>
+            <span>2015 — Present</span>
+            <p className="timeline-text">
+              Nemo enim ipsam voluptatem blanditiis praesentium voluptum delenit
+              atque corrupti, quos dolores et qvuas molestias exceptur.
+            </p>
+          </li>
+          <li className="timeline-item">
+            <h4 className="h4 timeline-item-title">Art director</h4>
+            <span>2013 — 2015</span>
+            <p className="timeline-text">
+              Nemo enims ipsam voluptatem, blanditiis praesentium voluptum
+              delenit atque corrupti, quos dolores et quas molestias exceptur.
+            </p>
+          </li>
+          <li className="timeline-item">
+            <h4 className="h4 timeline-item-title">Web designer</h4>
+            <span>2010 — 2013</span>
+            <p className="timeline-text">
+              Nemo enims ipsam voluptatem, blanditiis praesentium voluptum
+              delenit atque corrupti, quos dolores et quas molestias exceptur.
+            </p>
+          </li>
+        </ol>
+      </section>
+      {/* <section className="skill">
         <h3 className="h3 skills-title">My skills</h3>
         <ul className="skills-list content-card">
           <li className="skills-item">
